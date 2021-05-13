c4c44b82be37e5da7d8fb0e37d00323ff008d6a1
ayodejiayankola <ayodejiayankola@gmail.com>
2021-05-13 11:17:50 +0100

    setting up the rootview controller for the app

---
+++ InstagramAppClone/Controllers/MainTabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabBarController.swift
+//  InstagramAppClone
+//
+//  Created by Ayodeji Ayankola on 5/13/21.
+//
+
+import Foundation
+import UIKit
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+}
