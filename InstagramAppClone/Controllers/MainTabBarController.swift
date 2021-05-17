4ba7371adce302e2e34c9eb2d3fc183f9a6e50cd
ayodejiayankola <ayodejiayankola@gmail.com>
2021-05-17 11:56:31 +0100

    completed the tar bar menu with image icons

--- InstagramAppClone/Controllers/MainTabBarController.swift
+++ InstagramAppClone/Controllers/MainTabBarController.swift
@@ -12,29 +12,29 @@ class MainTabBarController: UITabBarController {
     // MARK:- Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-  
         configureViewController()
     }
     
     
     // MARK:- Helpers
-    
     func configureViewController() {
-        
         view.backgroundColor = .white
-        
-        let feed = FeedController()
-        
-        let search = SearchController()
-        
-        let profile = ProfileController()
-        
-        let imageSelector = ImageSelectorController()
-        
-        let notification = NotificationController()
-        
-        
+        //let feed = FeedController()
+        let feed = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "home_unselected"), selectedImage: #imageLiteral(resourceName: "home_unselected"), rootViewController: FeedController())
+        let search = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "search_unselected"), selectedImage: #imageLiteral(resourceName: "search_selected"), rootViewController: SearchController())
+        let profile = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "profile_unselected"), selectedImage: #imageLiteral(resourceName: "profile_selected"), rootViewController: ProfileController())
+        let imageSelector = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "plus_unselected"), selectedImage: #imageLiteral(resourceName: "plus_unselected"), rootViewController: ImageSelectorController())
+        let notification =  templateNavigationController(unselectedImage: #imageLiteral(resourceName: "like_unselected"), selectedImage: #imageLiteral(resourceName: "like_selected"), rootViewController: NotificationController())
+        tabBar.tintColor = .black
         viewControllers = [feed, search, profile, imageSelector, notification]
     }
-    
+        
+    func templateNavigationController(unselectedImage: UIImage , selectedImage: UIImage , rootViewController: UIViewController) -> UINavigationController {
+        let nav = UINavigationController(rootViewController: rootViewController)
+        nav.tabBarItem.image = unselectedImage
+        nav.tabBarItem.selectedImage = selectedImage
+        nav.navigationBar.tintColor = .black
+        return nav
+        
+    }
 }
