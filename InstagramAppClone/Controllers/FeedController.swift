44d614b533c113aecf8de039ce09155947a73e7b
ayodejiayankola <ayodejiayankola@gmail.com>
2021-05-25 14:43:43 +0100

    completed feeds ui

--- InstagramAppClone/Controllers/FeedController.swift
+++ InstagramAppClone/Controllers/FeedController.swift
@@ -17,8 +17,7 @@ class FeedController: UICollectionViewController {
         super.viewDidLoad()
         configureUI()
     }
-    
-    
+
     //MARK:- Helpers
     func configureUI() {
         collectionView.backgroundColor = .white
@@ -35,12 +34,16 @@ extension FeedController {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseAbleIdenfier, for: indexPath) as! FeedCell
         return cell
     }
-    
 }
 
 //MARK:- UICollectionViewDelegateFlowLayout
 extension FeedController: UICollectionViewDelegateFlowLayout{
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: 200)
+        let width = view.frame.width
+        var height = width + 8 + 40 + 8
+        height += 50
+        height += 60
+        
+        return CGSize(width: width, height: height)
     }
 }
